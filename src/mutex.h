11619d2214ea0241f1b40195e256fca7f56d035d
shanfeng0004 <shanfeng0004@163.com>
2018-07-26 23:51:29 +0800

    add mutex.h

---
+++ src/mutex.h
@@ -0,0 +1,19 @@
+#ifndef _LEVENT_MUTEX_H__
+#define _LEVENT_MUTEX_H__
+
+#include <boost/noncopyable.hpp>
+
+namespace levent
+{
+
+class Mutex : public boost::noncopyable
+{
+public:
+
+private:
+}
+
+
+}
+
+#endif // _LEVENT_MUTEX_H__
