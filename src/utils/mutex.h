0a8a2488c27f0204387a42c5b67f744322268203
shanfeng0004 <shanfeng0004@163.com>
2018-08-08 23:03:11 +0800

    add cmake

---
+++ src/utils/mutex.h
@@ -0,0 +1,56 @@
+#ifndef _LEVENT_MUTEX_H__
+#define _LEVENT_MUTEX_H__
+
+#include <boost/noncopyable.hpp>
+#include <pthread.h>
+
+namespace levent
+{
+
+class Mutex : public boost::noncopyable
+{
+public:
+    Mutex()
+    {
+        pthread_mutex_init(&mutex_, NULL);
+    }
+
+    ~Mutex()
+    {
+        pthread_mutex_destroy(&mutex_);
+    }
+
+    void Lock()
+    {
+        pthread_mutex_lock(&mutex_);
+    }
+
+    void UnLock()
+    {
+        pthread_mutex_unlock(&mutex_);
+    }
+
+private:
+    pthread_mutex_t mutex_;
+}
+
+class MutexGuard : public boost::noncopyable
+{
+public:
+    explicit MutexGuard(Mutex& mutex):mutex_(mutex)
+    {
+        mutex_.Lock();
+    }
+
+    ~MutexGuard()
+    {
+        mutex_.Unlock();
+    }
+
+private:
+    Mutex mutex_;
+}
+
+}
+
+#endif // _LEVENT_MUTEX_H__
